bafa5e751c8f2455f3ae05607b53e6c13729f836
Alex Remstedt <alexremstedt@AlexRemstedt>
2023-03-05 10:03:39 +0100

    Lange agenda uitgebreid.

--- alv_agenda/agenda.py
+++ alv_agenda/agenda.py
@@ -21,16 +21,16 @@ class Agenda:
         self.location = location
         self.standard_alv = alv
 
-        # Create header
-        with open('snippets/header.txt') as h:
-            header = h.read()
-            compiled_header = compile(header, '<header>', 'eval')
-            evaluated_header = eval(compiled_header)
-        self.doc.add_paragraph(evaluated_header)
+        self.header()
 
         # Create standard points
         if self.standard_alv:
             self.alv()
+
+    def header(self):
+        """Create header."""
+        evaluated_header = evaluate_paragraph('snippets/header')
+        self.doc.add_paragraph(evaluated_header)
 
     def alv(self):
         """Bouw standaard punten voor ALV."""
@@ -86,14 +86,68 @@ class Agenda:
 
 
 class LangeAgenda(Agenda):
-    """Uitbreiding op de normale Agenda"""
+    """Uitbreiding op de normale Agenda."""
 
     def __init__(self, agenda_punten, location):
-        super.__init__(agenda_punten, location)
+        super().__init__(self, agenda_punten, location)
 
-    def uitgewerkt_stuk(self):
-        """Maak een stuk voor de lange agenda."""
+    def header(self):
+        """Create header."""
+        evaluated_header = evaluate_paragraph('snippets/header_lang.txt')
+        self.doc.add_paragraph(evaluated_header)
+
+    def alv(self):
+        """Bouw standaard punten voor ALV."""
+        date_last_alv = input("Wanneer was de vorige ALV?")
+        standard_punten = [
+                # Opening
+                AgendaPunt("Opening", 1, 5, '19:03', text_location="opening"),
+
+                # Mededelingen
+                AgendaPunt("Mededelingen", 2, 5, '19:03',
+                           text_location="mededelingen"),
+                # Onderwijs
+                AgendaPunt("Mededelingen Onderwijs", 3, 5,
+                           '19:03', text_location="onderwijs"),
+
+                # Financieel
+                AgendaPunt("Mededelingen Financieel", 4, 5,
+                           '19:03', text_location="financieel"),
+                # Goedkeuring Notulen
+                AgendaPunt(
+                    f"Goedkeuring Notulen van ALV gehouden op {date_last_alv}",
+                    5, 5, '19:03', text_location="notulen")
+        ]
+        for title in standard_punten:
+            self.doc.add_heading(title, level=1, style='List Number')
+            par = evaluate_paragraph(f'snippets/lange_agenda/{title}')
+            self.doc.add_paragraph(par)
+
+
+def evaluate_paragraph(path):
+    with open(path) as p:
+        paragraph = p.read()
+        compiled_paragraph = compile(paragraph, '<paragraph>', 'eval')
+        evaluated_paragraph = eval(compiled_paragraph)
+    return evaluated_paragraph
+
+    # def match_stuk(self, agendapunt):
+    #     """Maak een stuk voor de lange agenda."""
+    #     match agendapunt.type:
+    #         case "mededeling":
+    #         case "":
+    #         case "exploitatie":
+    #         case "presentatie":
+
         # opties = [mededeling, begroting, exploitatie, presentatie]
         # if match met optie, dan pak optie-snippet, met invulling van
         # hierboven
-        return None
+        # return None
+
+
+def main():
+    x = LangeAgenda(['1', '2'], 'Thuis')
+
+
+if __name__ == "__main__":
+    main()
--- alv_agenda/agenda_punt.py
+++ alv_agenda/agenda_punt.py
@@ -4,10 +4,11 @@ from datetime import time
 class AgendaPunt:
     """AgendaPunt"""
 
-    def __init__(self, title, id, tijdsduur, begintijd):
+    def __init__(self, title, id, tijdsduur, begintijd, **kwargs):
         self.title = title
         self.tijdsduur = tijdsduur
         self.id = int(id) - 1
         [h, m] = [int(n) for n in begintijd.split(":")]
         self.begintijd = time(h, m)
         self.eindtijd = self.begintijd.minute + int(tijdsduur)
+        self.path = kwargs['text_location']
